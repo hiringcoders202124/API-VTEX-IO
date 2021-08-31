588bad53cea09b6fa2e8dac4c8d52f2b3ee7665a
Kaio Siqueira <kaio.siqueira@poli.ufrj.br>
2021-08-30 23:37:54 -0300

    upload node files

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,10 +1,15 @@
 import { IOClients } from '@vtex/api'
-
-import Status from './status'
+import { OMS } from '@vtex/clients'
+import Leads from './leads'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
-  public get status() {
-    return this.getOrSet('status', Status)
+
+  public get oms() {
+    return this.getOrSet('oms', OMS)
+  }
+
+  public get leads() {
+    return this.getOrSet('leads', Leads)
   }
 }
