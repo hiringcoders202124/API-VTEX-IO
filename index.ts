c65d3796f8dc8015b31eb057014679b5ee8fca54
Kaio Siqueira <kaio.siqueira@poli.ufrj.br>
2021-08-30 23:36:25 -0300

    files update node

--- index.ts
+++ index.ts
@@ -1,57 +1,15 @@
-import type { ClientsConfig, ServiceContext, RecorderState, EventContext } from '@vtex/api'
-import { Service } from '@vtex/api'
-import { Clients } from './clients'
-import { checkLeads } from './middlewares/checkLeads'
+import { IOClients } from '@vtex/api'
+import { OMS } from '@vtex/clients'
+import Leads from './leads'
 
-const TIMEOUT_MS = 800
+// Extend the default IOClients implementation with our own custom clients.
+export class Clients extends IOClients {
 
-
-// This is the configuration for clients available in `ctx.clients`.
-const clients: ClientsConfig<Clients> = {
-  // We pass our custom implementation of the clients bag, containing the Status client.
-  implementation: Clients,
-  options: {
-    // All IO Clients will be initialized with these options, unless otherwise specified.
-    default: {
-      retries: 2,
-      timeout: TIMEOUT_MS,
-    },
-  },
-}
-
-declare global {
-  // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
-  type Context = ServiceContext<Clients, State>
-
-  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
-  interface State extends RecorderState {
-    code: number,
-    id: string
+  public get oms() {
+    return this.getOrSet('oms', OMS)
   }
-  interface StatusChangeContext extends EventContext<Clients> {
-    body: {
-      domain: string
-      orderId: string
-      currentState: string
-      lastState: string
-      currentChangeDate: string
-      lastChangeDate: string
-    }
-  }
-  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
-  interface State extends RecorderState { }
-}
 
-// Export a service that defines route handlers and client options.
-export default new Service({
-  clients,
-  events: {
-    checkLeads,
-  },
-  routes: {
-    // `status` is the route ID from service.json. It maps to an array of middlewares (or a single handler).
-    //   getOrder: method({
-    //     GET: [getOrder],
-    //   }),
-  },
-})
+  public get leads() {
+    return this.getOrSet('leads', Leads)
+  }
+}
